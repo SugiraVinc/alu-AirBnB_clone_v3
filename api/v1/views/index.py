821691bbbd8692d866f12c61f163bd2869b7868a
SugiraVinc <sugiravincent11@gmail.com>
2023-04-06 22:08:58 +0000

    index

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""Create a route on the object app_views that returns a JSON: "status":OK """
+from api.v1.views import app_views
+from flask import jsonify
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+
+@app_views.route('/status', methods=['GET'])
+def status():
+    """Status of the API"""
+    print("status function called")
+    return jsonify({'status': 'OK'})
