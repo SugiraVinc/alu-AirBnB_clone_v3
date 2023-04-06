575a7cfcf106ad31f2e61ddc442720dc1b977548
MaikarfiJesse <j.maikarfi1@alustudent.com>
2023-04-07 01:07:57 +0200

    fix

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -5,6 +5,8 @@ Create a blueprint
 from flask import Blueprint
 
 app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
+
+"""import views after defining app_views blueprint"""
 from api.v1.views.index import *
 from api.v1.views.amenities import *
 from api.v1.views.states import *
