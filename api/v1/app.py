02822d00f1d90257cdd100b2300288e3e227f2c2
SugiraVinc <sugiravincent11@gmail.com>
2023-04-06 22:27:24 +0000

    app info

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,23 +1,30 @@
 #!/usr/bin/python3
-"""This module defines a Flask application that serves a RESTful API"""
-
-import os
+"""
+This module defines a Flask application that serves a RESTful API
+"""
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
-from flask import Flask, jsonify
+from flask_cors import CORS
+from os import environ
 
 app = Flask(__name__)
-
-app.register_blueprint(app_views, url_prefix='/api/v1')
+app.register_blueprint(app_views)
+cors = CORS(app, resources={r"/api/v1/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
-def teardown_storage(exception):
-    """Closes the storage"""
+def teardown_app(exception):
     storage.close()
 
 
 @app.errorhandler(404)
-def error_not_found(error):
-    """404 error"""
-    return jsonify({"error": "Not found"}
+def page_not_found(e):
+    """handler for routes that don't exist"""
+    return jsonify({"error": "Not found"}), 404
+
+
+if __name__ == '__main__':
+    host = environ.get("HBNB_API_HOST", "0.0.0.0")
+    port = environ.get("HBNB_API_PORT", 5000)
+    app.run(host=host, port=port, threaded=True)
