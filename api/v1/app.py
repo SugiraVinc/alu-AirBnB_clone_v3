a648423758e007338c635568783b03cce83e92c3
MaikarfiJesse <j.maikarfi1@alustudent.com>
2023-04-07 09:45:05 +0200

    app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,7 +5,7 @@ This module defines a Flask application that serves a RESTful API
 import os
 from flask import Flask
 from api.v1.views import app_views
-from models import storage 
+from models import storage
 
 app = Flask(__name__)
 app.register_blueprint(app_views, url_prefix='/api/v1')
