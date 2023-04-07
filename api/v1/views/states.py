05583f3f2e5c9b8a7599fe303d94a027c76a57df
MaikarfiJesse <j.maikarfi1@alustudent.com>
2023-04-07 11:59:38 +0200

    states.py

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -7,7 +7,6 @@ from flask import jsonify, request, abort
 from api.v1.views import app_views
 from models import storage
 from models.state import State
-from flask import jsonify, abort
 
 
 @app_views.route('/states', methods=['GET'])
