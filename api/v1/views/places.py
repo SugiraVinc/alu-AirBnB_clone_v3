bd2ad8166c7e2be6797029f76ae66d689e90a166
MaikarfiJesse <j.maikarfi1@alustudent.com>
2023-04-07 15:00:57 +0200

    places.py

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -1,1 +1,62 @@
 #!/usr/bin/python3
+"""Endpoint for places"""
+from api.v1.views import app_views
+from flask import abort, jsonify, request
+from models import storage
+from models.place import Place
+from models.user import User
+from models.city import City
+
+
+@app_views.route('/cities/<city_id>/places', methods=['GET'])
+def get_places(city_id):
+    """Retrieves the list of all Place objects of a City"""
+    city = storage.get(City, city_id)
+    if city is None:
+        abort(404)
+    places = [place.to_dict() for place in city.places]
+    return jsonify(places)
+
+
+@app_views.route('/places/<place_id>', methods=['GET'])
+def get_place(place_id):
+    """Retrieves a Place object"""
+    place = storage.get(Place, place_id)
+    if place is None:
+        abort(404)
+    return jsonify(place.to_dict())
+
+
+@app_views.route('/places/<place_id>', methods=['DELETE'])
+def delete_place(place_id):
+    """Deletes a Place object"""
+    place = storage.get(Place, place_id)
+    if place is None:
+        abort(404)
+    storage.delete(place)
+    storage.save()
+    return jsonify({})
+
+
+@app_views.route('/cities/<city_id>/places', methods=['POST'])
+def create_place(city_id):
+    """Creates a Place"""
+    city = storage.get(City, city_id)
+    if city is None:
+        abort(404)
+    if not request.json:
+        abort(400, 'Not a JSON')
+    if 'user_id' not in request.json:
+        abort(400, 'Missing user_id')
+    user_id = request.json['user_id']
+    user = storage.get(User, user_id)
+    if user is None:
+        abort(404)
+    if 'name' not in request.json:
+        abort(400, 'Missing name')
+    place = Place(**request.json)
+    place.city_id = city_id
+    place.user_id = user_id
+    storage.new(place)
+    storage.save()
+    return jsonify(place.to_dict()), 201
