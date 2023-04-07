5ea4764beacc0bb5982ccbcb8db6173d4366a66d
MaikarfiJesse <j.maikarfi1@alustudent.com>
2023-04-07 13:35:12 +0200

    db_storage.py

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -74,7 +74,7 @@ class DBStorage:
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
-    
+
     def get(self, cls, id_):
         """
         Retrieve an object from storage based on its class and ID
